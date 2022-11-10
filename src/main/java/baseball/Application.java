55aa69e6c97d5128fc932149f857789f734a2899
TaeMin <xoals3094@naver.com>
2022-11-10 11:14:24 +0900

    feat(Application): 유효값 확인 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static boolean isValidData(List<Integer> player){
+        for(int i = 0; i < player.size(); i++){
+            int number = player.get(i);
+            if(i == player.lastIndexOf(number)){
+                throw new IllegalArgumentException();
+            }
+        }
+        return true;
     }
 }
