c42abe45b50b45457d543a3e3ef5b0adef7c278c
TaeMin <xoals3094@naver.com>
2022-11-10 10:19:56 +0900

    feat(Judgment): 일치하는 숫자의 개수를 구하는 기능 구현

---
+++ src/main/java/baseball/Judgment.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.List;
+
+public class Judgment {
+    public int countMatchNumber(List<Integer> computer, List<Integer> player){
+        int count = 0;
+        for(int i = 0; i < player.size(); i++){
+            int number = player.get(i);
+            if(computer.contains(number)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
