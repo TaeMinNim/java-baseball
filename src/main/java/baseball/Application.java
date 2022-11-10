a94e72a28907c574fd01476c878abf01f6e54b97
TaeMin <xoals3094@naver.com>
2022-11-10 12:00:11 +0900

    feat(Application): 기능 통합

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,28 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        Referee referee = new Referee();
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        String choice = "1";
+        while(choice.equals("1")) {
+            List<Integer> computer = numberGenerator.createNumber();
+            String result = "";
+            while (!result.equals("3스트라이크")) {
+                System.out.println("숫자를 입력해주세요 : ");
+                String input = Console.readLine();
+                List<Integer> player = toIntegerList(input);
+                isValidData(player);
+
+                result = referee.compare(computer, player);
+                System.out.println(result);
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            choice = Console.readLine();
+        }
     }
 
     public static List<Integer> toIntegerList(String input){
